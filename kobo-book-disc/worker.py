bd3c5cf41f6a98600064cbcf82988d28c5ff4764
Pochun <pochun26@gmail.com>
2024-04-12 16:18:59 +0800

    Error handling: Authentication error

--- kobo-book-disc/worker.py
+++ kobo-book-disc/worker.py
@@ -4,6 +4,11 @@ from utils.helper import extract_price, discount_calculator
 from utils.book import Book
 
 
+class AuthenticationError(Exception):
+    def __str__(self) -> str:
+        return "Authentication Error: check your session"
+
+
 class Worker:
     def __init__(self, page=1) -> None:
         self.page = page
@@ -22,6 +27,9 @@ class Worker:
         r = requests.post(url, json=payload, headers=headers)
         if r.status_code == 200:
             data = r.json()
+            if data.get("redirectUrl"):
+                app.logger.error("Authentication Error")
+                raise AuthenticationError
             self.total_pages = data.get("TotalNumPages")
             for d in data.get("Items", []):
                 if d["WasPrice"] is not None:
